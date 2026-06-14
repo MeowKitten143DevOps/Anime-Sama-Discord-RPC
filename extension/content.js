ff5c9e0c8c0e9ba104e20b3d35a1a0e29b536c00
MeowKitten143DevOps <232105587+MeowKitten143DevOps@users.noreply.github.com>
2026-06-15 07:03:13 +0900

    updated whole content.js to have the anime sama support rpc
    added :
                        else if (host == "animesama") {
    
                            browser.runtime.sendMessage({
                                "cmd": "update",
                                "args": {
                                    "type": "update",
                                    "host": "animesama",
                                    "details": anime,
                                    "state": `Episode (${cur_ep} of ${max_ep})${season ? " - " + season : ""}`,
                                    "anilist": url.anilist
                                }
                            });
    
                        }
    
                    });
    
                }
    
                else if (response == false && last_stand == true) {
                    browser.runtime.sendMessage({ "cmd": "clear" });
                }
    
                last_stand = response;
            });
    }

--- extension/content.js
+++ extension/content.js
@@ -1,54 +1,81 @@
-var last_stand = false
-function communicateToBackground(host, anime, cur_ep=null, max_ep=null, season=null, episode=null) {
-    // first send check-request to check if tab is audible
-    browser.runtime.sendMessage({"cmd": "check"})
-    .then((response) => {
-        console.info("Playing_state: ", response)
-        if (response==true && last_stand==false) {
-            // if tab is now audible and before not -> start rpc
-            browser.storage.local.get("anilist").then(
-                url => {
-                    if (url.anilist == undefined) {url.anilist = ""}
-                    // start RPC with current data when audio start
+var last_stand = false;
+
+function communicateToBackground(host, anime, cur_ep = null, max_ep = null, season = null, episode = null) {
+
+    browser.runtime.sendMessage({ "cmd": "check" })
+        .then((response) => {
+
+            console.info("Playing_state: ", response);
+
+            if (response == true && last_stand == false) {
+
+                browser.storage.local.get("anilist").then((url) => {
+
+                    if (url.anilist == undefined) {
+                        url.anilist = "";
+                    }
+
                     if (host == "aniworld") {
+
                         browser.runtime.sendMessage({
-                            "cmd": "update", 
-                            "args": { 
+                            "cmd": "update",
+                            "args": {
                                 "type": "update",
-                                "host": "aniworld", 
-                                "details": anime, 
+                                "host": "aniworld",
+                                "details": anime,
                                 "state": `Episode (${cur_ep} of ${max_ep}), Season ${season}`,
                                 "anilist": url.anilist
                             }
-                        })
+                        });
+
                     }
-                    if (host == "crunchyroll") {
+
+                    else if (host == "crunchyroll") {
+
                         browser.runtime.sendMessage({
-                            "cmd": "update", 
-                            "args": { 
+                            "cmd": "update",
+                            "args": {
                                 "type": "update",
-                                "host": "crunchyroll", 
-                                "details": anime, 
+                                "host": "crunchyroll",
+                                "details": anime,
                                 "state": `Episode: ${episode}`,
                                 "anilist": url.anilist
                             }
-                        })
+                        });
+
                     }
-                }
-            )
-        } else if (response==false && last_stand==true) {
-            // clear RPC when audio stopped
-            browser.runtime.sendMessage({"cmd": "clear"})
-        }
-        last_stand = response;
-    })
+
+                    else if (host == "animesama") {
+
+                        browser.runtime.sendMessage({
+                            "cmd": "update",
+                            "args": {
+                                "type": "update",
+                                "host": "animesama",
+                                "details": anime,
+                                "state": `Episode (${cur_ep} of ${max_ep})${season ? " - " + season : ""}`,
+                                "anilist": url.anilist
+                            }
+                        });
+
+                    }
+
+                });
+
+            }
+
+            else if (response == false && last_stand == true) {
+                browser.runtime.sendMessage({ "cmd": "clear" });
+            }
+
+            last_stand = response;
+        });
 }
 
-/* Observing-Function to wait for an element to appear */
+
+// wait for element to appear
 function waitElement(selector, callback) {
-    // New MutationObserver to observe changes in DOM
     var observer = new MutationObserver(mutations => {
-        // For each mutation occurs in DOM, check if element is present
         mutations.forEach(mutation => {
             var element = mutation.target.querySelector(selector);
             if (element) {
@@ -57,32 +84,39 @@ function waitElement(selector, callback) {
             }
         });
     });
-    // start observing
+
     observer.observe(document.documentElement, {
         childList: true,
         subtree: true
     });
 }
 
-window.onload = ()=>{
+
+window.onload = () => {
+
+    // aniworld
     if (document.location.host == "aniworld.to") {
-        console.clear()
-        streamBox = document.getElementsByClassName("inSiteWebStream")
+
+        console.clear();
+
+        let streamBox = document.getElementsByClassName("inSiteWebStream");
+
         if (streamBox.length > 0) {
-            infos = document.getElementsByClassName("hosterSiteTitle")[0]
-            if (infos.getAttribute("data-season") != "0") { // if season is selected, not film
-                // get current stream data
+
+            let infos = document.getElementsByClassName("hosterSiteTitle")[0];
+
+            if (infos.getAttribute("data-season") != "0") {
+
                 var anime = document.getElementsByClassName("series-title")[0].children[0].innerText;
-                    season = infos.getAttribute("data-season");
-                    cur_ep = document.getElementsByClassName("active")[1].innerText;
-                    max_ep = document.getElementsByClassName("active")[1].parentElement.parentElement.childElementCount -1;
+                var season = infos.getAttribute("data-season");
+                var cur_ep = document.getElementsByClassName("active")[1].innerText;
+                var max_ep = document.getElementsByClassName("active")[1].parentElement.parentElement.childElementCount - 1;
 
                 console.log("Anime: ", anime);
                 console.log("Season: ", season);
                 console.log("Cur Episode: ", cur_ep);
                 console.log("Max Episode: ", max_ep);
 
-                // save current stream data to local-storage for sync-function from popup.js
                 browser.storage.local.set({
                     "cur_stream_data": {
                         "anime": anime,
@@ -90,37 +124,41 @@ window.onload = ()=>{
                         "tot_ep": max_ep,
                         "season": season
                     }
-                })
+                });
 
-                // check every 5 seconds if audio is playing
-                checkPlaying = setInterval(() => {
-                    // first check if auto_rpc is enabled
-                    browser.storage.local.get('auto_rpc').then(
-                        (item) => {
-                            // if undefined -> set initial to enabled
-                            if (item.auto_rpc == undefined) {
-                                browser.storage.local.set({"auto_rpc": "enabled"})
-                                item.auto_rpc = 'enabled'
-                            }
-                            // if enabled start requesting with background.js
-                            if (item.auto_rpc == 'enabled') {communicateToBackground("aniworld", anime, cur_ep, max_ep, season)}
+                let checkPlaying = setInterval(() => {
+
+                    browser.storage.local.get('auto_rpc').then((item) => {
+
+                        if (item.auto_rpc == undefined) {
+                            browser.storage.local.set({ "auto_rpc": "enabled" });
+                            item.auto_rpc = "enabled";
                         }
-                    )
+
+                        if (item.auto_rpc == 'enabled') {
+                            communicateToBackground("aniworld", anime, cur_ep, max_ep, season);
+                        }
+                    });
+
                 }, 5000);
             }
         }
     }
 
+
+    // crunchyrol
     if (document.location.host == "www.crunchyroll.com") {
-        console.clear()
-        waitElement('.erc-current-media-info', (infobox) => {
-            var anime = document.querySelector("a.show-title-link").innerText
-                episode = document.querySelector(".erc-current-media-info h1.title").innerText
 
-            console.log("Anime: ", anime)
-            console.log("Episode: ", episode)
+        console.clear();
 
-            // save current stream data to local-storage for sync-function from popup.js
+        waitElement('.erc-current-media-info', () => {
+
+            var anime = document.querySelector("a.show-title-link").innerText;
+            var episode = document.querySelector(".erc-current-media-info h1.title").innerText;
+
+            console.log("Anime: ", anime);
+            console.log("Episode: ", episode);
+
             browser.storage.local.set({
                 "cur_stream_data": {
                     "anime": anime,
@@ -128,39 +166,96 @@ window.onload = ()=>{
                     "tot_ep": "",
                     "season": ""
                 }
-            })
+            });
 
-            // check every 5 seconds if audio is playing
-            checkPlaying = setInterval(() => {
-                // first check if auto_rpc is enabled
-                browser.storage.local.get('auto_rpc').then(
-                    (item) => {
-                        // if undefined -> set initial to enabled
-                        if (item.auto_rpc == undefined) {
-                            browser.storage.local.set({"auto_rpc": "enabled"})
-                            item.auto_rpc = 'enabled'
-                        }
-                        // if enabled start requesting with background.js
-                        if (item.auto_rpc == 'enabled') {communicateToBackground("crunchyroll", anime, null, null, null, episode)}
+            let checkPlaying = setInterval(() => {
+
+                browser.storage.local.get('auto_rpc').then((item) => {
+
+                    if (item.auto_rpc == undefined) {
+                        browser.storage.local.set({ "auto_rpc": "enabled" });
+                        item.auto_rpc = "enabled";
                     }
-                )
-            }, 5000);
-        })
-    }
-}
 
-// if tab/window was closed stop RPC (only if auto_rpc is enabled)
-window.onbeforeunload = () => {
-    // first check if auto_rpc is enabled
-    browser.storage.local.get('auto_rpc').then(
-        (item) => {
-            // if undefined -> set initial to enabled
-            if (item.auto_rpc == undefined) {
-                browser.storage.local.set({"auto_rpc": "enabled"})
-                item.auto_rpc = 'enabled'
+                    if (item.auto_rpc == 'enabled') {
+                        communicateToBackground("crunchyroll", anime, null, null, null, episode);
+                    }
+                });
+
+            }, 5000);
+
+        });
+    }
+
+
+    // animesama
+    if (document.location.host.includes("anime-sama")) {
+
+        console.clear();
+
+        const anime =
+            document.getElementById("titreOeuvre")?.innerText || "";
+
+        const season =
+            document.getElementById("avOeuvre")?.innerText || "";
+
+        const episode =
+            document.getElementById("selectEpisodes")?.value || "";
+
+        const totalEpisodes =
+            document.getElementById("selectEpisodes")?.options.length || "";
+
+        console.log("Anime:", anime);
+        console.log("Season:", season);
+        console.log("Episode:", episode);
+        console.log("Total Episodes:", totalEpisodes);
+
+        browser.storage.local.set({
+            "cur_stream_data": {
+                "anime": anime,
+                "cur_ep": episode.replace("Episode ", ""),
+                "tot_ep": totalEpisodes,
+                "season": season
             }
-            // if enabled start -> stop contingently running RPC
-            if (item.auto_rpc == 'enabled') {browser.runtime.sendMessage({"cmd": "clear"})}
+        });
+
+        let checkPlaying = setInterval(() => {
+
+            browser.storage.local.get('auto_rpc').then((item) => {
+
+                if (item.auto_rpc == undefined) {
+                    browser.storage.local.set({ "auto_rpc": "enabled" });
+                    item.auto_rpc = "enabled";
+                }
+
+                if (item.auto_rpc == "enabled") {
+                    communicateToBackground(
+                        "animesama",
+                        anime,
+                        episode.replace("Episode ", ""),
+                        totalEpisodes,
+                        season
+                    );
+                }
+            });
+
+        }, 5000);
+    }
+};
+
+
+// stop rpc on close
+window.onbeforeunload = () => {
+
+    browser.storage.local.get('auto_rpc').then((item) => {
+
+        if (item.auto_rpc == undefined) {
+            browser.storage.local.set({ "auto_rpc": "enabled" });
+            item.auto_rpc = "enabled";
         }
-    )
-}
+
+        if (item.auto_rpc == 'enabled') {
+            browser.runtime.sendMessage({ "cmd": "clear" });
+        }
+    });
+};
